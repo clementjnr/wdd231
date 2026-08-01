a3ef10087b41572c45642fdaa3a4a6daf43e4541
Paul Clement <69912818+clementjnr@users.noreply.github.com>
2026-08-01 13:02:36 +0100

    Update thankyou.js

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,1 +1,46 @@
+const params = new URLSearchParams(window.location.search);
 
+const firstName = params.get("firstName") || "";
+const lastName = params.get("lastName") || "";
+const email = params.get("email") || "";
+const phone = params.get("phone") || "";
+const organization = params.get("organization") || "";
+const timestamp = params.get("timestamp") || "";
+
+const details = document.querySelector("#application-details");
+
+details.innerHTML = `
+    <div class="application-result">
+        <h3>Application Information</h3>
+
+        <p>
+            <strong>First Name:</strong>
+            ${firstName}
+        </p>
+
+        <p>
+            <strong>Last Name:</strong>
+            ${lastName}
+        </p>
+
+        <p>
+            <strong>Email:</strong>
+            ${email}
+        </p>
+
+        <p>
+            <strong>Phone:</strong>
+            ${phone}
+        </p>
+
+        <p>
+            <strong>Business / Organization:</strong>
+            ${organization}
+        </p>
+
+        <p>
+            <strong>Application Timestamp:</strong>
+            ${timestamp}
+        </p>
+    </div>
+`;
