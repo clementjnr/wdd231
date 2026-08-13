35634714ce46993126725e49241f26603c687073
Paul Clement <69912818+clementjnr@users.noreply.github.com>
2026-08-13 20:31:00 +0100

    Update main.js

--- travel-agency/scripts/main.js
+++ travel-agency/scripts/main.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD:travel-agency/scripts/main.js
-const menuButton = document.querySelector("#menu-button");
-const navigation = document.querySelector("#navigation");
-
-menuButton.addEventListener("click", () => {
-    const isOpen = navigation.classList.toggle("open");
-
-    menuButton.setAttribute("aria-expanded", isOpen);
-    menuButton.setAttribute(
-        "aria-label",
-        isOpen ? "Close navigation menu" : "Open navigation menu"
-    );
-});
-
-const year = document.querySelector("#current-year");
-
-if (year) {
-    year.textContent = new Date().getFullYear();
-}
-
-const lastModified = document.querySelector("#last-modified");
-
-if (lastModified) {
-    lastModified.textContent = `Last modified: ${document.lastModified}`;
-}
-=======
 import { showDestinationModal } from "./modules/data.js";
 
 const destinationContainer =
@@ -307,4 +281,3 @@ if (videoLink) {
 
 
 getDestinations();
->>>>>>> 6a227e50430d6416d835316d26c8a3cd30691136:travel/scripts/main.js
